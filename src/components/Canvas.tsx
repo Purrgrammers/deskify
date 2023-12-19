e583e05fbace9f45423273fbc8c5b2761fb3ec0d
Felicia <felicia.lyden@gmail.com>
2023-12-19 21:07:45 +0100

    saving transform values on resize

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { KonvaEventObject } from "konva/lib/Node";
-import { Shape, ShapeConfig } from "konva/lib/Shape";
-import { useRef } from "react";
+import { DragEvent, useRef } from "react";
 import { useState } from "react";
 import { Stage, Layer, Rect, Transformer } from "react-konva";
 
 const Canvas = () => {
-  const [rooms, setRooms] = useState([{ id: 1, x: 50, y: 50 }]);
+  const [rooms, setRooms] = useState([{ id: 1, x: 50, y: 50, width: 50, height: 50, scaleX: 1, scaleY: 1},
+    { id: 2, x: 376.9999999999998, y: 165.99999999999983, width: 50, height: 50, scaleX: 3.360000000000005, scaleY: 3.2200000000000037}]);
   const [desks, setDesks] = useState([{ id: 1, x: 50, y: 50 }]);
-  const [focus, setFocus] = useState<Shape<ShapeConfig> | null>(null);
 
   const trRef = useRef(null);
 
@@ -22,41 +21,60 @@ const Canvas = () => {
     console.log(rooms[draggedElementIndex]);
   };
 
-  const handleFocus = (e: KonvaEventObject<MouseEvent>) => {
-    console.log(e.target)
-    setFocus(e.target as Shape<ShapeConfig>);
+  const handleTransform = (e: KonvaEventObject<Event>, id: number) => {
+    console.log(e.target.attrs)
+    const transformedElementIndex = rooms.findIndex((room) => room.id === id);
+    rooms[transformedElementIndex] = {
+      ...rooms[transformedElementIndex],
+      scaleX: e.target.attrs.scaleX,
+      scaleY: e.target.attrs.scaleY,
+      x: e.target.attrs.x,
+      y: e.target.attrs.y
+    };
 
+  };
+
+  const handleFocus = (e: KonvaEventObject<MouseEvent>) => {
     // @ts-expect-error just let me do it
     trRef.current?.nodes([e.target]);
     // @ts-expect-error just let me do it
     trRef.current?.getLayer().batchDraw();
   };
+  
+  const handleBlur = (e: KonvaEventObject<MouseEvent>) => {
+    console.log(e.target)
+  }
+
+
 
   return (
     <>
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
-        className="canvas"
       >
         <Layer>
           {rooms.map((room) => (
             <Rect
               key={room.id}
-              width={50}
-              height={50}
+              width={room.width}
+              height={room.height}
+              scaleX={room.scaleX}
+              scaleY={room.scaleY}
+              x={room.x}
+              y={room.y}
               stroke="black"
               draggable
               onClick={(e) => handleFocus(e)}
               onDragStart={() => {
-                setRooms([...rooms, { id: rooms.length + 1, x: 50, y: 50 }]);
+                setRooms([...rooms, { id: rooms.length + 1, x: 50, y: 50, width: 50, height: 50, scaleX: 1, scaleY: 1 }]);
               }}
               onDragEnd={(e) => {
                 handleDragged(e, room.id);
               }}
+              onTransformEnd={(e) => handleTransform(e, room.id)}
             />
           ))}
-          {focus && (
             <Transformer
               ref={trRef}
               rotateEnabled={false}
@@ -66,9 +84,9 @@ const Canvas = () => {
               anchorStrokeWidth={1}
               anchorSize={10}
               keepRatio={false}
+              flipEnabled={false}
               ignoreStroke={true}
             />
-          )}
         </Layer>
       </Stage>
     </>
