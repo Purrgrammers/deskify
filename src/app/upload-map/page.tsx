5bcf3d674e29ac171cd6964b89b752160713bac1
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-12-19 16:22:34 +0100

    feat: Preview uploaded picture

--- src/app/upload-map/page.tsx
+++ src/app/upload-map/page.tsx
@@ -2,8 +2,10 @@ import DropZone from "@/components/DropZone";
 const Upload = () => {
   return (
     <>
-      <h1></h1>
-      <DropZone />
+      <h1 className="text-xl mt-4">Uppload your Office floor plan</h1>
+      <div className="m-3 mt-7">
+        <DropZone />
+      </div>
     </>
   );
 };
