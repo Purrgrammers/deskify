36e3aaba63faee16946143cd8a0a1c69ee314514
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-12-19 13:21:11 +0100

    feat: Uploading of map component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,10 @@
-'use client'
+"use client";
 
 import { createClient } from "@supabase/supabase-js";
 import { useEffect } from "react";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-type User = {
-  id: number;
-  created_at: string;
-  name: string;
-};
-
-
-const Home = async () => {
-
-  useEffect(() => {
-    const supabase = createClient(supabaseUrl, supabaseAnonKey);
-    const getDb =  async () => {
-      const { data, error } = await supabase.from("Users").select();
-      console.log(data, error)
-    }
-    getDb()
-  })
-
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-      {/* <p>{(data as unknown as User).name}</p> */}
-    </main>
-  );
-};
+const Home = () => {};
 
 export default Home;
---
+++ src/app/upload-map/page.tsx
@@ -0,0 +1,11 @@
+import DropZone from "@/components/DropZone";
+const Upload = () => {
+  return (
+    <>
+      <h1></h1>
+      <DropZone />
+    </>
+  );
+};
+
+export default Upload;
