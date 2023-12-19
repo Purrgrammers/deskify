19db76500260226d4a44373e0471540eacb44091
Felicia <felicia.lyden@gmail.com>
2023-12-19 13:19:13 +0100

    Add ActionCard component

---
+++ src/components/ActionCard.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+type ActionCardProps = {
+  img: string;
+  title: string;
+  description: string;
+  btnText: string;
+  reverse?: boolean
+};
+
+const ActionCard = ({ img, title, description, btnText, reverse }: ActionCardProps) => {
+
+    const imgPlacement = reverse? 'flex-row-reverse': 'flex-row'
+    const btnPlacement = reverse? 'place-self-start': 'place-self-end'
+
+  return (
+    <Card className="w-[350px] p-4">
+      <CardContent className={`flex gap-4 ${imgPlacement}`}>
+      <Image src={img} alt="image" width={100} height={200} className="border-solid border-black border-2"/>
+        <div className="flex flex-col">
+          <CardTitle>{title}</CardTitle>
+          <CardDescription className="my-2">{description}</CardDescription>
+          <Button className={`${btnPlacement}`}>{btnText}</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ActionCard;
