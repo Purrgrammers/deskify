8951b366dd735f7a77dc04721a3f260840430ae9
Felicia <felicia.lyden@gmail.com>
2023-12-20 10:20:12 +0100

    add buttons and text to create map page

--- src/app/create-map/page.tsx
+++ src/app/create-map/page.tsx
@@ -1,5 +1,6 @@
 
 'use client';
+import { Button } from '@/components/ui/button';
 // import Canvas from '@/components/Canvas';
 import dynamic from 'next/dynamic';
 
@@ -9,7 +10,13 @@ const Canvas = dynamic(() => import('@/components/Canvas'), {
 
 const CreateMap = ()=> {
   return (
+    <>
+    <h1 className='text-2xl'>Create your office map</h1>
+    <p>Drag and drop bookable rooms and desks onto your map</p>
     <Canvas></Canvas>
+    <Button variant="secondary">Back</Button>
+    <Button>Create map</Button>
+    </>
   );
 }
 
