0fc70b7e48768c37a631ce7908abc3df8cc2ad7b
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-12-21 10:49:20 +0100

    feat: Fetch all bookings

---
+++ src/app/my-bookings/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { createClient } from "@supabase/supabase-js";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+type Booking = {
+  id: string;
+  date: string;
+  userId: number;
+  deskId: number;
+};
+
+const Bookings = () => {
+  const [bookings, setBookings] = useState<Booking[]>([]);
+
+  useEffect(() => {
+    const fetchAllBookings = async () => {
+      const { data, error } = await supabase.from("Bookings").select();
+      if (error) {
+        console.log("Fetching problem:", error);
+        return;
+      }
+      setBookings(data);
+      console.log("Those are my bookings", data);
+    };
+    fetchAllBookings();
+  }, []);
+  return (
+    <>
+      <h1 className="text-2xl my-6 mx-4">Bookings</h1>
+    </>
+  );
+};
+
+export default Bookings;
