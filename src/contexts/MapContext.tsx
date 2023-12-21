e854174da903fbae6c03d3c3846a19bd8ce76783
Felicia <felicia.lyden@gmail.com>
2023-12-21 15:06:19 +0100

    fix server error issue on page load

--- src/contexts/MapContext.tsx
+++ src/contexts/MapContext.tsx
@@ -1,6 +1,11 @@
-import { supabase } from "@/components/BookingCanvas";
+import { createClient } from "@supabase/supabase-js";
 import { createContext, useState } from "react";
 import toast from "react-hot-toast";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 type MapContextProviderProps = {
   children: React.ReactNode;
