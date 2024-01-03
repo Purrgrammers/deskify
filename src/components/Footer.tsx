39f754c041ff86fe040acdbbecfe779d3ab3c337
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2024-01-03 13:58:15 +0100

    style: Website version for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,11 @@ import { Github } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-around px-2 mx-2 mt-8 py-4 pt-2 border-t border-gray text-xs font-light text-slate-400">
-      <p className="w-4/12 text-center">
+    <footer className="flex justify-around px-2 mx-2 mt-8 py-4 pt-2 border-t border-gray text-xs font-light text-slate-400 md:mx-6">
+      <p className="w-4/12 lg:w-4/12 text-center md:text-left">
         Project created at SALT Bootcamp 2024.
       </p>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col md:flex-row md:gap-3 gap-1">
         <a href="https://github.com/felicialyden" target="_blank">
           <div className="flex items-center gap-2">
             <Github size={20} />
