d58ca7393045c22e61ab04796b7e94f3d7f99ef2
Felicia <felicia.lyden@gmail.com>
2024-01-05 13:12:45 +0100

    add section on upload map page to add info about facility

--- src/app/upload-map/page.tsx
+++ src/app/upload-map/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
 import DropZone from "@/components/DropZone";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { MapContextProvider } from "@/contexts/MapContext";
 const Upload = () => {
   return (
     <MapContextProvider>
       <div className="flex flex-col mt-12 mb-8 items-center max-w-2xl mx-auto">
+        <div className="text-left w-full mb-8">
+          <h1 className="text-2xl font-semibold m-4 mb-8">
+            Add some info about your facility
+          </h1>
+          <div className="mx-4 my-2">
+            <p>
+              This will be shown in the booking page.
+            </p>
+          </div>
+          <div className="mx-4">
+          <Label htmlFor="address" className="ml-px mt-px">Address</Label>
+          <Input type="text" id="address" name="input-address"></Input>
+          <Label htmlFor="floor" className="ml-px mt-px">Floor number</Label>
+          <Input type="number" id="floor" name="input-floor"></Input>
+          </div>
+        </div>
         <div className="text-left">
           <h1 className="text-2xl font-semibold m-4 mb-8">
-            Upload your Office floor plan
+            Upload your office floor plan
           </h1>
           <div className="mx-4 my-2">
             <p>
