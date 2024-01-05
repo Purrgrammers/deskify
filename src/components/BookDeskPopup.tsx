f1447a3dc406a964ee0c23a11c8ab88ca82fb160
Felicia <felicia.lyden@gmail.com>
2024-01-05 10:37:08 +0100

    position book desk popup above element

--- src/components/BookDeskPopup.tsx
+++ src/components/BookDeskPopup.tsx
@@ -1,17 +1,24 @@
 import { MapContext } from "@/contexts/MapContext";
 import { Pencil, Trash2 } from "lucide-react";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import RoomInfoForm from "./RoomInfoForm";
 import { Button } from "./ui/button";
 
 
 const BookDeskPopup = () => {
-    const { focusElement, bookDesk, bookRoom } = useContext(MapContext)
-    const [editMode, setEditMode] = useState(false)
-    
+    const { focusElement, updateFocusElement, bookDesk, bookRoom } = useContext(MapContext)
+    const [ popupHeight, setPopupHeight] = useState(0)
     const canvas = document.querySelector('#BookDeskStage')
     const offsetLeft = (canvas as HTMLDivElement)?.offsetLeft
     const offsetTop =  (canvas as HTMLDivElement)?.offsetTop
+    const popupElement = document.querySelector('#bookDeskPopup') as HTMLDivElement
+
+    useEffect(() => {
+      if(popupElement){
+        console.log(popupElement.offsetHeight)
+        setPopupHeight(popupElement.offsetHeight)
+      }
+    }, [popupElement, focusElement])
 
     const handleBooking = () => {
       if(focusElement?.type === 'Room') {
@@ -25,7 +32,8 @@ const BookDeskPopup = () => {
     <div
     style={{
         position: "absolute",
-        top: (focusElement?.y || 10) + offsetTop - 30,
+        maxWidth: 180,
+        top: (focusElement?.y || 10) + offsetTop - (popupHeight + 3),
         left: (focusElement?.x || 10) + offsetLeft,
         padding: "5px 5px",
         borderRadius: "3px",
@@ -33,13 +41,16 @@ const BookDeskPopup = () => {
         zIndex: 10,
         backgroundColor: "white"
       }}
+      id="bookDeskPopup"
     >
       <div>
       <p className="text-sm">{`${focusElement?.type} ${focusElement?.id}: `} <span className={focusElement?.booked? 'text-red-600': 'text-green-600'}>{focusElement?.booked? 'Booked': 'Available'}</span></p>
       {focusElement?.seats && <p className="text-xs">Number of seats: {focusElement.seats}</p>}
       {focusElement?.additionalInfo && <p className="text-xs">Additional information: {focusElement.additionalInfo}</p>}
+      <div className="flex gap-2 justify-end">
+      <Button className="mt-2" size='xs' variant='secondary' onClick={() => updateFocusElement(undefined)}>Close</Button>
       <Button disabled={focusElement?.booked? true : false} className="mt-2" size='xs' onClick={handleBooking}>Book</Button>
-
+      </div>
       </div>
     </div>
   );
