c451a26ad7ad84afda9dc9020b5475ea7c9ef944
Felicia <felicia.lyden@gmail.com>
2024-01-07 12:12:07 +0100

    clarify info text for facility info

--- src/app/upload-map/page.tsx
+++ src/app/upload-map/page.tsx
@@ -9,26 +9,36 @@ const Upload = () => {
     <MapContextProvider>
       <div className="flex flex-col mt-12 mb-8 items-center max-w-2xl mx-auto">
         <div className="text-left w-full mb-8">
-          <h1 className="text-2xl font-semibold m-4 mb-6">
+          <h1 className="text-2xl font-semibold m-4 mb-4">
             Add some info about your map
           </h1>
           <div className="mx-4 my-2">
-            <p className="mb-4">
-              This will be shown in the booking page.
+            <p className="mb-6">
+              Provide location name and floor number to help you
+              and your users navigate between maps. This step is required.
             </p>
           </div>
           <div className="mx-4 flex flex-col gap-2">
-          <Label htmlFor="location" className="ml-px mt-px">Location</Label>
-          <Input type="text" id="location" name="input-location" className="mb-2"></Input>
-          <Label htmlFor="floor" className="ml-px mt-px" >Floor number</Label>
-          <Input type="text" id="floor" name="input-floor"></Input>
+            <Label htmlFor="location" className="ml-px mt-px">
+              Location
+            </Label>
+            <Input
+              type="text"
+              id="location"
+              name="input-location"
+              className="mb-2"
+            ></Input>
+            <Label htmlFor="floor" className="ml-px mt-px">
+              Floor number
+            </Label>
+            <Input type="text" id="floor" name="input-floor"></Input>
           </div>
         </div>
         <div className="text-left w-full">
-          <h1 className="text-2xl font-semibold m-4 mb-6">
+          <h1 className="text-2xl font-semibold m-4 mb-4">
             Upload your office floor plan
           </h1>
-          <div className="mx-4 my-2">
+          <div className="mx-4 my-4">
             <p>
               Upload your floor plan image for an effortless mapping of your
               office space. Alternatively, you can choose to skip this step and
@@ -36,7 +46,7 @@ const Upload = () => {
             </p>
           </div>
           <div className="w-full">
-            <div className="m-4 mt-5 flex flex-col justify-center w-full">
+            <div className="mt-6 flex flex-col justify-center w-full">
               <DropZone />
             </div>
           </div>
